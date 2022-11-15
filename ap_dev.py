8b94561c618ec48ebc6a5d11c90eb2ea3f34efcc
Jarum77 <jamesrumbold77@gmail.com>
2022-11-15 11:19:52 +0000

    Updated ap_dev to import ceers from read_ceers
    Also switched fourgemax to be calculated from END_min

--- ap_dev.py
+++ ap_dev.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov  8 11:20:13 2022
-
-@author: george
-"""
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from scipy.io import readsav
@@ -12,46 +5,33 @@ import numpy as np
 from mpmath import mp
 import NDpredict as ndp
 from aperture_func import Halo_Masses
+from read_ceers import read_in_ceers
 
-mass_data = readsav('./data/ceers1/ceers1.me_Z02_calz.fastpp.delltaugt8.5.bc03.ch.save')
-rshift_data = readsav('./data/ceers1/ceers1_v0.2_redshift.save')
-
-mass_vals = mass_data['sed_lmass']
-ids = mass_data['sed_id']
-zvals = rshift_data['z_best']
-
-
-#prog_vals = [[],[]]
-#not_prog_vals = [[],[]]
-#growth_curve = [[],[],[],[]] #z_val, median, upper, lower
+ceers_data = read_in_ceers('1 2 3 6',True)
+zvals = ceers_data[:,1]
+mvals = ceers_data[:,2]
 
 zi = 0.
 z = 0.
 M0 = 10.5   # All stellar masses are assumed to be logarithmic
 N0= -2.806814232365416  #Calculated from ndp.getnum for M0 = 10.5 and z = 0
 
-#strip any -99 fluxes and same indexs for IDs and Masses
-nintynines=np.full(len(zvals),-99)
-
-ids=ids[~np.equal(nintynines,zvals)]
-mass_vals=mass_vals[~np.equal(nintynines,zvals)]
-zvals=zvals[~np.equal(nintynines,zvals)]
-
 FOURGE_MINS=[]
 FOURGE_MAXS=[]
 mins=[]
 maxes=[]
 
-#create curve for interpolation
-x=np.linspace(0, np.ndarray.max(zvals)+1, num=71, endpoint=False)
-#x=np.linspace(0, , num=71, endpoint=False)
+x=np.linspace(0, np.ndarray.max(zvals)+1, num=71, endpoint=False)  #Generate equally spaced Z-index from 0 to the largest in our data + 1
+
 for i in x:
-    ymax=ndp.evolvingN( N0 , 0 , np.round(i, 5 ) ) + ndp.sigmaN( N0 , 0, np.round(i , 5 ) )
-    ymin=ndp.evolvingN( N0 , 0 , np.round(i, 5 ) ) - ndp.sigmaN( N0 , 0, np.round(i, 5 ) )
-    fourgemax=ndp.getmass(ymax, round( i , 5 ) , massfunc="zfourge")
-    fourgemin=ndp.getmass(ymin, round( i , 5 ) , massfunc="zfourge")
-    maxes=np.append(maxes,fourgemax)
-    mins=np.append(mins,fourgemin)
+    END_max = ndp.evolvingN( N0 , 0 , i ) + ndp.sigmaN( N0 , 0, i )     #Calculate the Evolving Number Density + 1σ
+    END_min=ndp.evolvingN( N0 , 0 , i ) - ndp.sigmaN( N0 , 0, i )     #Calculate the Evolving Number Density - 1σ
+    fourgemax=ndp.getmass(END_min, i , massfunc="zfourge")            #Calculate the Median fourge mass + 1σ
+    fourgemin=ndp.getmass(END_max, i , massfunc="zfourge")            #Calculate the Median fourge mass - 1σ
+
+    print(f"At redshift={i}, The fourgemax={fourgemax} and the fourgemin={fourgemin}")
+    maxes.append(fourgemax)
+    mins.append(fourgemin)
     
 fMax=interp1d(x,mins)
 fMin=interp1d(x,maxes)
@@ -63,17 +43,17 @@ upper_bounds=fMax(zvals)
 zeros=np.zeros(len(lower_bounds))
 
 
-zvals1=zvals[np.less(zeros, (np.subtract(mass_vals,lower_bounds)))]
-mass_vals1=mass_vals[np.less(zeros, (np.subtract(mass_vals,lower_bounds)))]
-zeros1=zeros[np.less(zeros, (np.subtract(mass_vals,lower_bounds)))]
-upper_bounds1=upper_bounds[np.less(zeros, (np.subtract(mass_vals,lower_bounds)))]
-lower_bounds1=lower_bounds[np.less(zeros, (np.subtract(mass_vals,lower_bounds)))]
+zvals1=zvals[np.less(zeros, (np.subtract(mvals,lower_bounds)))]
+mass_vals1=mvals[np.less(zeros, (np.subtract(mvals,lower_bounds)))]
+zeros1=zeros[np.less(zeros, (np.subtract(mvals,lower_bounds)))]
+upper_bounds1=upper_bounds[np.less(zeros, (np.subtract(mvals,lower_bounds)))]
+lower_bounds1=lower_bounds[np.less(zeros, (np.subtract(mvals,lower_bounds)))]
 
 zvals=zvals1[np.less(zeros1, (np.subtract(upper_bounds1,mass_vals1)))]
-mass_vals=mass_vals1[np.less(zeros1, (np.subtract(upper_bounds1,mass_vals1)))]
+mvals=mass_vals1[np.less(zeros1, (np.subtract(upper_bounds1,mass_vals1)))]
 
 
-halo_masses=Halo_Masses(zvals,mass_vals)
+halo_masses=Halo_Masses(zvals,mvals)
 print(halo_masses)
 #for i in 
 
