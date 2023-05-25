4cf94a572f266a8b71a063776ed474e7d357d76c
Jarum77 <jamesrumbold77@gmail.com>
2023-05-25 10:53:18 +0100

    Update plot
    Updated the plot to combine the stellar mass vs halo mass and virial radius vs halo mass

--- virial_radius.py
+++ virial_radius.py
@@ -7,7 +7,7 @@ from astropy import units as u
 from astropy.constants import M_sun, pc
 from astropy.cosmology import WMAP9 as cosmo
 
-def find_virial_radius(mass,rshift):
+def find_virial_radius(stell_mass, halo_mass, zvals):
     
     omega_m0=cosmo.Om(0)
     omega_de0=cosmo.Ode(0)
@@ -15,7 +15,7 @@ def find_virial_radius(mass,rshift):
 
     G=scipy.constants.G
 
-    hz=np.add(rshift,1)       
+    hz=np.add(zvals,1)       
     hz=np.power(hz,3)    
     hz=np.multiply(hz,omega_m0)
     hz=np.add(omega_de0, hz)
@@ -31,7 +31,7 @@ def find_virial_radius(mass,rshift):
 
     hz_sq_100=np.multiply(100,(np.power(hz,2)))
     
-    Mh_vals = np.multiply(np.power(10,mass),M_sun)
+    Mh_vals = np.multiply(np.power(10,halo_mass),M_sun)
 
     numerator=np.multiply(Mh_vals, G)
     
@@ -39,33 +39,59 @@ def find_virial_radius(mass,rshift):
 
     vradius_kpc=np.divide(vradius_m.value,pc.value*10**3)
 
-    kpc_per_arcmin = cosmo.kpc_proper_per_arcmin(rshift)
+    kpc_per_arcmin = cosmo.kpc_proper_per_arcmin(zvals)
 
     vradius_arcmin = np.divide(vradius_kpc, kpc_per_arcmin)
     vradius_deg = np.divide(vradius_arcmin, 60)
 
+    '''
+        Plot the Progenitor virial radius and halo halo_mass
+    '''
 
-    # fig, ax = plt.subplots()
+    # fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(10, 6))
+    # fig.subplots_adjust(wspace=0.3)
+    # cm = plt.cm.get_cmap('viridis')
+    # ## Halo Mass
+
     # plt.rcParams["font.family"] = "serif"
     # font = {'fontname':'serif'} 
-    # plt.grid(color='silver', linestyle='--', linewidth=1, zorder=1)
-    # plt.scatter(mass, vradius_kpc, c=rshift,s=10,marker=".", zorder=2)
+    # ax1.set_yticks(np.arange(7.5, 11.5, 0.5))
+    # ax1.set_xticks(np.arange(10.5,12.5,0.25))
+    # ax1.grid(color='silver', linestyle='--', linewidth=1, zorder=1)
 
-    # plt.xlabel("$Log_{10}$($M_{h}$/$M_{☉}$)", fontdict=font)
-    # plt.ylabel("$R_{virial}$  $(kpc)$", **font)
+    # ax1.scatter(halo_mass, stell_mass, c=zvals, cmap=cm, s=15,marker=".", zorder=2,vmax=4)
+    # ax1.set_xlabel("Halo Mass [$Log_{10}$($M_{DM}$/$M_{☉}$)]",**font)
+    # ax1.set_ylabel("Stellar Mass [$Log_{10}$($M_{*}$/$M_{☉}$)]",**font)
 
-    # cbar = plt.colorbar()
-    # cbar.ax.set_ylabel("Redshift")
-    # cbar.ax.set_yticks([np.amin(rshift),0.5,1,1.5,2,2.5,3,3.5,4])
-    # plt.xlim(10.6,12.2)
-    # x_ticklabels = plt.gca().get_xticklabels()
-    # y_ticklabels = plt.gca().get_yticklabels()
+    # x_ticklabels = ax1.get_xticklabels()
+    # y_ticklabels = ax1.get_yticklabels()
+    # for tick_label in (x_ticklabels + y_ticklabels):
+    #     tick_label.set_fontname("serif")
+    # ax1.set_xlim(10.75,12.5)
+    # ax1.set_ylim(8,11)
+
+
+    # ## Virial Radius
+    # ax2.grid(color='silver', linestyle='--', linewidth=1, zorder=1)
+    # sc2 = ax2.scatter(halo_mass, vradius_kpc, c=zvals,s=15, cmap=cm, marker=".", zorder=2, vmax=4)
+
+    # ax2.set_xlabel("Halo Mass [$Log_{10}$($M_{DM}$/$M_{☉}$)]", fontdict=font)
+    # ax2.set_ylabel("Virial Radius [kpc]", **font)
+    # print(np.amax(vradius_kpc))
+    # print(np.amin(vradius_kpc))
+
+    # ticks_colbar = [0.5,1,1.5,2,2.5,3,3.5,4]
+    # cbar = fig.colorbar(sc2, ax=[ax1, ax2], orientation="horizontal", ticks=ticks_colbar,shrink=0.7, label="Redshift", cmap=cm, pad=0.15)
+    # ax2.set_xlim(10.75,12.5)
+    # ax2.set_ylim(0,250)
+    # x_ticklabels = ax2.get_xticklabels()
+    # y_ticklabels = ax2.get_yticklabels()
 
     # for tick_label in (x_ticklabels + y_ticklabels):
     #     tick_label.set_fontname("serif")
 
-    # plt.savefig('plots/virial_radius.png', dpi=300)
-    # plt.ylim(20,200)
+    # plt.savefig('plots/virial_radius_halo_mass.png', dpi=300, bbox_inches='tight' )
+
     # plt.show()
      
     return vradius_deg.value
